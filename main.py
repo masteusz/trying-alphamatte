f8fb36c98cf256c9e4b36c9689ac2ecf536b7448
Mateusz Stankiewicz <mateusz@mateusz.at>
2025-05-15 23:32:17 +0100

    feat: integrate YOLO model for image segmentation
    - Add YOLO model to perform segmentation on input images.
    - Process and refine segmentation masks with resizing, erosion, and outline detection.
    - Display segmentation results using OpenCV.

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+
 def main():
-    print("Hello from trying-alphamatte!")
+    model = YOLO("yolo11n-seg.pt")
+    image = cv2.imread("images/1.png")
+    h, w, _ = image.shape
+    mask_image = np.zeros((h, w), dtype=np.uint8)
+    result = model(image)[0]
+
+    masks = result.masks.data.cpu()
+
+    for seg in masks.data.cpu().numpy():
+        seg = cv2.resize(seg, (w, h))
+        seg = seg.astype(np.uint8) * 255
+        mask_image += seg
+
+    kernel = np.ones((7, 7))
+    smaller_mask_image = mask_image.copy()
+    smaller_mask_image = cv2.erode(smaller_mask_image, kernel, iterations=5)
+    outline = mask_image != smaller_mask_image
+    outline = outline.astype(np.uint8)
+
+    cv2.imshow("mask", mask_image)
+    cv2.waitKey()
+
+    mask_image[outline > 0] = 127
+
+    cv2.imshow("mask", mask_image)
+    cv2.waitKey()
+
+    pass
 
 
 if __name__ == "__main__":
